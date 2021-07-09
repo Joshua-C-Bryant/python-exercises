d2a55dc3e4b06669a5897f2dd6a3b467170150a9
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-07-09 09:58:04 -0500

    updated versions

--- data_type_and_variables.py
+++
@@ -1,7 +0,0 @@
-def total_price(n):
-    return n*3
-total_price(3)
-total_price(5)
-total_price(1)
-
-
---
+++ my_first_notebook.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greeting = 'Hello World!'
+print(greeting)
+
+
+# In[2]:
+
+
+2*2
+
+
+# In[3]:
+
+
+8/2
+
+
+# ### Some jupyter shortcuts include:
+# #### A = insert cell above
+# #### B = insert cell below
+# #### Z = undo cell deletion
