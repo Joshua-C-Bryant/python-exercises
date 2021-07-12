4b941a5960e895fea7289b15c4429f86faaa23ca
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-07-12 16:01:47 -0500

    initial commit

---
+++ function_exercises.py
@@ -0,0 +1,14 @@
+#Define a function named is_two. 
+# It should accept one input and return True if the passed input is either the number or 
+# the string 2, False otherwise.
+
+def is_two(n):
+    if n == 2 or n == "2"
+    else:
+        return False
+
+is_two(2)
+
+#Define a function named is_vowel. 
+# It should return True if the passed string is a vowel, False otherwise.
+
