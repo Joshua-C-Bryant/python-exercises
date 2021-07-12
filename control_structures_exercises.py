b9a322d668350221bb82ec38388ebd4ea1e14f2d
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-07-12 14:48:48 -0500

    updated answers

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -41,13 +41,235 @@ weekly_paycheck(40)
 
 # While
 
+#Create an integer variable i with a value of 5
+
 i = 5
+
+#Create a while loop that runs so long as i is less than or equal to 15
 while i <= 15:
     print(i)
     i += 1
 
-
-i =0
+#Create a while loop that will count by 2's starting with 0 and ending at 100. 
+# Follow each number with a new line.
+i = 0
 while i in range(0, 101):
     print(i)
     i += 2
+
+#Alter your loop to count backwards by 5's from 100 to -10.
+i = 100
+while i > -11:
+    print(i)
+    i = (i - 5)
+
+#Create a while loop that starts at 2, and displays the number squared 
+# on each line while the number is less than 1,000,000.
+
+i = 2
+while i < 1000000:
+    print(i)
+    i = i**2
+
+#Write a loop that uses print to create the output shown below.
+#100
+#95
+#90
+#85
+#80
+#75
+#70
+#65
+#60
+#55
+#50
+#45
+#40
+#35
+#30
+#25
+#20
+#15
+#10
+#5
+
+i = 100
+while i > 0:
+    print(i)
+    i = i -5
+
+# For Loops
+
+#Write some code that prompts the user for a number, 
+# then shows a multiplication table up through 10 for that number.
+
+#For example, if the user enters 7, your program should output:
+# 7 x 1 = 7
+# 7 x 2 = 14
+# 7 x 3 = 21
+# 7 x 4 = 28
+# 7 x 5 = 35
+# 7 x 6 = 42
+# 7 x 7 = 49
+# 7 x 8 = 56
+# 7 x 9 = 63
+# 7 x 10 = 70
+
+i = input("What's your favorite number? ")
+num = 1
+for  num in range (1,10):
+    product = (int(i)*num)
+    print(i,"x",num,"=",product,)
+    num = num + 1
+
+#Create a for loop that uses print to create the output shown below.
+#1
+#22
+#333
+#4444
+#55555
+#666666
+#7777777
+#88888888
+#999999999
+
+n = int(input("How many rows? "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+#An Easier way
+
+for r in range(1,10):
+    print(str(r) * r)
+
+
+# Prompt the user for an odd number between 1 and 50. 
+# Use a loop and a break statement to continue prompting the user if they enter invalid input. 
+# (Hint: use the isdigit method on strings to determine this). 
+# Use a loop and the continue statement to output all the odd numbers between 1 and 50, 
+# except for the number the user entered.
+
+
+# STILL NEEDS WORK
+n = int(input("Please enter an odd number between 1 and 50"))
+for i in range (1,51,2):
+    if n % 2 == 0:
+        print(input("Invalid input"))
+    print("Here is an odd number: ", i)
+
+###Answer
+print("Enter a whole odd number between 1 and 50")
+pick = input()
+
+if pick.isnumeric() == False:
+    print()
+    print("Please enter a WHOLE NUMBER!")
+    print()
+
+    continue
+
+else:
+    pick = int(pick)
+
+if (pick < 51) and (pick %2 != 0):
+    print()
+    print(f"Thank you! Your number is: {pick}")
+    print()
+
+    break
+
+else:
+
+    print()
+    print("Please Enter a number less than 50 and odd!")
+    print()
+
+for number in range(1,50):
+    if number % 2 == 0:
+        continue
+    print("Here is an odd number:", number)
+
+
+pick = 5
+
+for number in range(1,50):
+    if number % 2 == 0:
+        continue
+    if number == pick:
+        print("Yikes! Skipping number:", number)
+        continue
+    print("Here is an odd number:", number)
+
+#The input function can be used to prompt for input and use that input in your python code. 
+# Prompt the user to enter a positive number and write a loop that counts from 0 to that number. 
+# (Hints: first make sure that the value the user entered is a valid number, also 
+# note that the input function returns a string, so you'll need to convert this to a numeric type.)
+
+# STILL NEEDS WORK
+n = int(input("Please enter an even number "))
+for i in range (1,n+1):
+    if n % 2 == 0:
+        print(i)
+        i = i + 1
+    else:
+        int(input("Please enter a valid number "))
+        continue
+
+###ANSWER
+
+print("Enter a positive whole number!")
+pick = input()
+
+if pick.isnumeric() == False:
+    print()
+    print("Please enter a POSITIVE WHOLE NUMBER!")
+    print()
+
+else:
+
+    pick = int(pick)
+
+    print()
+    print(f"Thank you! Your number is: {pick}"
+    print()
+
+    break #This gets us out of the function
+
+for r in range (0,pick +1):
+    print(r)
+
+
+
+#Write a program that prompts the user for a positive integer. 
+# Next write a loop that prints out the numbers from the number the user entered down to 1.
+
+n = int(input("Please enter a postive integer "))
+for i in range (n ,1):
+    print i
+    i = i-1
+
+
+#FIZZ BUZZ
+
+for r in range(1,101):
+    if r % 15 == 0:
+        print("FizzBuzz")
+    elif r % 3 == 0:
+        print("Fizz")
+    elif r % 5 == 0:
+        print("Buzz")
+    else:
+        print(r)
+
+
+
+
+
+
+
+
+
+
+
--- list_comprehensions.py
+++ list_comprehensions.py
@@ -22,23 +22,38 @@ uppercased_fruits = []
 for fruit in fruits:
     uppercased_fruits.append(fruit.upper())
 
-uppercased_fruits
+print(uppercased_fruits)
 
 # Exercise 2 - create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
 capitalized_fruits = []
 for fruit in fruits:
     capitalized_fruits.append(fruit.capitalize())
 
-capitalized_fruits
+print(capitalized_fruits)
 
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
+# STILL NEEDS WORK
+
 fruits_with_more_than_two_vowels = []
 for fruit in fruits:
-    if count(letter in fruit in list('aeiou')):
-        fruits_with_more_than_two_vowels.append(fruit)
+    for vowel in ("a","e","i","o","u"):
+        if fruit.count(vowel) > 2:
+            fruits_with_more_than_two_vowels.append(fruit)
 
-fruits_with_more_than_two_vowels            
+print(fruits_with_more_than_two_vowels)
+
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+# STILL NEEDS WORK
+
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+
+fruits_with_two_vowels = []
+for fruit in fruits:
+    for char in fruit:
+        if char in 'aeiou':
+            fruits_with_two_vowels.append(fruit)
+
+print(fruits_with_two_vowels)
 
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
 fruits_with_more_than_five_characters = []
@@ -48,12 +63,14 @@ for fruit in fruits:
 
 fruits_with_more_than_five_characters
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
+
 fruits_with_exactly_five_characters = []
 for fruit in fruits:
     if len(fruit) == 5:
         fruits_with_exactly_five_characters.append(fruit)
 
 fruits_with_exactly_five_characters
+
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
 fruits_with_less_than_five_characters = []
 for fruit in fruits:
@@ -91,14 +108,57 @@ odd_numbers
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
 
+positive_numbers = []
+for n in numbers:
+    if n > 0:
+        positive_numbers.append(n)
+
+positive_numbers
+
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
 
+negative_numbers = []
+for n in numbers:
+    if n < 0:
+        negative_numbers.append(n)
+
+negative_numbers
+
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
+
+numbers_with_2_or_more_numbers = []
+for n in numbers:
+    if abs(n) > 9:
+        numbers_with_2_or_more_numbers.append(n)
+
+numbers_with_2_or_more_numbers
 
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
 
+numbers_squared = []
+for n in numbers:
+    numbers_squared.append(n**2)
+
+numbers_squared
+
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
+
+odd_negative_numbers = []
+for n in numbers:
+    if n < 0 and n % 2 != 0:
+        odd_negative_numbers.append(n)
+
+odd_negative_numbers
 
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
 
+numbers_plus_5 = []
+for n in numbers:
+    numbers_plus_5.append(n + 5)
+
+numbers_plus_5
+
 # BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
+
+primes = []
